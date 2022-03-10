30f2bc9b44155689a7ab8366fba7cea647cd7242
Patricia <paiogna@gmail.com>
2022-03-10 16:30:24 -0300

    Primera entrega del Proyecto Final

--- main.js
+++ main.js
@@ -1,16 +1,24 @@
 const carrito = [];
 let comprar = "";
+const totalAPagar = [];
+let total1 = 0;
 
 
-function agregarAlCarrito (producto,stockProducto) {
+function agregarAlCarrito (producto,stockProducto, precio) {
     do {
         let cantidad = prompt('¿Cuántas unidades del producto queres?');
             if (cantidad<=stockProducto) {
                 comprar = prompt("Estas seguro que quieres agregar "+cantidad+ " producto/s al carrito? s / n");
                     if (comprar==="s"){
                         alert("Agregaste " + cantidad + " unidades del producto " + producto + " a tu carrito");
-                        carrito.push(producto);
+                        const productos =
+                            {nombre: producto, cantidad: cantidad, precio: precio};
+                        carrito.push(productos.cantidad, productos.nombre, productos.precio);
                         console.log(carrito);
+                        totalAPagar.push(productos.precio*cantidad);
+                        console.log(totalAPagar);
+                        const total = totalAPagar.reduce((acumulador, elemento) => acumulador + elemento, 0);
+                        return total1 = total;
                     }
                     else {
                         alert("Intentalo nuevamente");
@@ -24,6 +32,6 @@ function agregarAlCarrito (producto,stockProducto) {
 }
 
 function carritoFinal () {
-    alert("Tienes los siguientes productos agregados a tu carrito: \r" + carrito.join("\r"));
+    alert("Tienes los siguientes productos agregados a tu carrito: \r" + carrito.join("\r") + " \r Total a pagar: " + total1);
 }
 
