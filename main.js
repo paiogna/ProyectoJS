7ca02f60d1c89ff1474ee6b35eba9c4ecf5c7284
Patricia <paiogna@gmail.com>
2022-04-02 19:29:07 -0300

    Optimizando el Proyecto Final

--- main.js
+++ main.js
@@ -1,10 +1,13 @@
 let carrito = [];
-if (localStorage.getItem("carrito") == null) {
-    carrito = [];
-} else {
-    const carritoStorage = JSON.parse(localStorage.getItem("carrito"));
-    carrito = carritoStorage;
-}
+// if (localStorage.getItem("carrito") == null) {
+//     carrito = [];
+// } else {
+//     const carritoStorage = JSON.parse(localStorage.getItem("carrito"));
+//     carrito = carritoStorage;
+// }
+
+const carritoStorage = JSON.parse(localStorage.getItem("carrito"))
+localStorage.getItem("carrito") == null ? carrito = [] : carrito = carritoStorage
 
 
 let comprar = "";
