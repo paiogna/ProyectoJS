10ef82f16b37178230ebb22df5cb869f499a5552
Patricia <paiogna@gmail.com>
2022-04-14 09:13:27 -0300

    Entrega Proyecto Final

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 let carrito = [];
+
+// ---- CICLO ANTES DE USAR EL OPERADOR TERNARIO ----
 // if (localStorage.getItem("carrito") == null) {
 //     carrito = [];
 // } else {
@@ -36,30 +38,25 @@ function agregarAlCarrito (producto, stock, precio, id) {
             text: `Agregaste al carrito el producto ${producto} por ${cantidad} unidades`,
                 style: {
                 background: "pink",
+                color: "black",
                 }
             }).showToast();
-        //alert("Agregaste " + cantidad + " unidades del producto " + producto + " a tu carrito por $" + precio*cantidad);
             function Productos (cantidadP, tituloP, precioP) {
                 this.cantidad = cantidadP;     
                 this.titulo = tituloP;
                 this.precio = precioP;
                 }
             const productO = new Productos (cantidad, producto, precio);
-            //console.log(productO);
             carrito.push(productO);
-            //console.log(carrito);
             localStorage.setItem("carrito", JSON.stringify(carrito));
             totalAPagar.push(precio*cantidad);
             localStorage.setItem("totalAPagar", JSON.stringify(totalAPagar));
-            //console.log(totalAPagar);
             const total = totalAPagar.reduce((acumulador, elemento) => acumulador + elemento, 0);
             return total1 = total;
     } else {
-            //alert("No tenemos stock suficiente del producto " + producto + " por el momento");
             Swal.fire({
                 title: 'Atención!',
                 text: 'No tenemos stock suficiente del producto seleccionado',
-                //icon: 'error',
                 imageUrl: 'imagen/logo.png',
                 imageWidth: 70,
                 imageHeight: 70,
@@ -88,14 +85,14 @@ productos.forEach (element => {
     card.id = element.id;
     card.style = "width: 18rem;"
     card.innerHTML = `
-    <img class="card-img-top" src="${element.imagen}" alt="Card image cap">
-    <div class="card-body">
+    <img class="card-img-top" src="${element.imagen}" alt="Card image cap" style="border-radius: 10px;">
+    <div class="card-body" >
         <h5 class="card-title">${element.titulo}</h5>
         <p class="card-text">$${element.precio}</p>
         <p class="card-text">Stock:${element.stock}</p>
         <label class="" for="">Cantidad:</label>
         <input class="cantidad" type="text" name="" id="cantidad${element.id}"></input>
-        <button onclick="agregarAlCarrito('${element.titulo}','${element.stock}', '${element.precio}','${element.id}')">Agregar al carrito</button>
+        <button style="border: 2px solid pink; border-radius: 10px; background-color: pink; color: black; font-size: 20px;" onclick="agregarAlCarrito('${element.titulo}','${element.stock}', '${element.precio}','${element.id}')">Agregar al carrito</button>
     </div>`
     prueba.appendChild(card);
 });
@@ -105,51 +102,57 @@ const cart = document.querySelector('.txtCarrito')
 const totalCart = document.querySelector('.totalCart')
 const cerrarCart = document.querySelector('.cerrarCart')
 
-function cerrarCarts () {
-    totalCart.remove();
-    cart.remove();
-    cerrarCart.remove();
-}
+const cartModal = document.querySelector('.modal-bodyProd')
+const totalCartModal = document.querySelector('.totalCartModal')
+
+const prodTabla = document.querySelector('.tbody')
 
 
 function carritoFinal () {
     carrito.forEach (element => {
-        let cartProd = document.createElement('div')
-        cartProd.innerHTML = `
-        <div class="card-body">
-            <h5 class="card-title">${element.titulo}, precio $${element.precio}, Cantidad: ${element.cantidad}</h5> 
-        </div>`
-        cart.appendChild(cartProd);
+        // let cartProd = document.createElement('div')
+        // cartProd.innerHTML = `
+        // <div class="card-body">
+        //     <h5 class="card-title">${element.titulo}, precio $${element.precio}, Cantidad: ${element.cantidad}</h5> 
+        // </div>`
+        // cartModal.appendChild(cartProd);
+        let tableProd = document.createElement('tr')
+        tableProd.innerHTML = `
+                <td>${element.cantidad}</td>
+                <td>${element.titulo}</td>
+                <td>$${element.precio}</td>
+            `
+        prodTabla.appendChild(tableProd);
     });
-    totalCart.innerHTML = "El total a pagar es $ " + total1;
-    cerrarCart.innerHTML = `<button onclick="cerrarCarts()">Cerrar carrito</button>`
+    totalCartModal.innerHTML = "El total a pagar es $ " + total1;
 }
 
+                        
+
+// ---- PRUEBA DE API RICK AND MORTY ----
 // const API_URL = 'https://rickandmortyapi.com/api';
 // const ENDPOINT_LISTADO_PERSONAJES = '/character';
 // const PARAMS_LISTADO_PERSONAJES = '/1,2,3,4,5,6';
+// const rickAndMorty = document.querySelector('.rickAndMorty')
 
-
-const rickAndMorty = document.querySelector('.rickAndMorty')
-
-fetch ("https://rickandmortyapi.com/api/character/1,2,3,4,5,242")
-.then ((response) => response.json())
-.then ((data) => {
-    data.forEach (element => {
-        let card = document.createElement('div')
-        card.id = element.id;
-        card.style = "width: 18rem;"
-        card.innerHTML = `
-        <img class="card-img-top" src="${element.image}" alt="Card image cap">
-        <div class="card-body">
-        <h5 class="card-title">Nombre: ${element.name}</h5>
-        <h6 class="card-title">Status: ${element.status}</h6>
-        <h6 class="card-title">Especie: ${element.species}</h6>
-        <h6 class="card-title">Origen: ${element.origin.name}</h6>
-        </div>`
-        rickAndMorty.appendChild(card);
-    })
-})
+// fetch ("https://rickandmortyapi.com/api/character/1,2,3,4,5,242")
+// .then ((response) => response.json())
+// .then ((data) => {
+//     data.forEach (element => {
+//         let card = document.createElement('div')
+//         card.id = element.id;
+//         card.style = "width: 18rem;"
+//         card.innerHTML = `
+//         <img class="card-img-top" src="${element.image}" alt="Card image cap">
+//         <div class="card-body">
+//         <h5 class="card-title">Nombre: ${element.name}</h5>
+//         <h6 class="card-title">Status: ${element.status}</h6>
+//         <h6 class="card-title">Especie: ${element.species}</h6>
+//         <h6 class="card-title">Origen: ${element.origin.name}</h6>
+//         </div>`
+//         rickAndMorty.appendChild(card);
+//     })
+// })
 
 // fetch ("https://rickandmortyapi.com/api/character/?page=13")
 // .then ((response) => response.json())
